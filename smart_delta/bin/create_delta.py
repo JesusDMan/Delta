33133f9b1ee32471225358a714a5699dfacdf23a
Yuval Mantin <mantin.work@gmail.com>
2024-02-24 22:51:29 +0200

    Added more options in argparse

--- smart_delta/bin/create_delta.py
+++ smart_delta/bin/create_delta.py
@@ -5,16 +5,22 @@ from typing import Optional
 from smart_delta.src import delta_generator
 
 
-def create_delta_file(filepath_1: str, filepath_2: str, delta_filepath: str, base_dir: Optional[str] = None):
+def create_delta_file(filepath_1: str, filepath_2: str, delta_filepath: str, max_diff_length: Optional[int],
+                      min_length_for_fit: Optional[int], base_dir: Optional[str] = None):
     if base_dir:
         os.chdir(base_dir)
 
     with open(filepath_1, "r") as f:
-        data_1 = f.read()
+        data_0 = f.read()
     with open(filepath_2, "r") as f:
-        data_2 = f.read()
+        data_1 = f.read()
 
-    delta_ = delta_generator.DeltaGenerator(data_1, data_2)
+    delta_ = delta_generator.DeltaGenerator(
+        data_0=data_0,
+        data_1=data_1,
+        max_diff_length=max_diff_length,
+        min_length_for_fit=min_length_for_fit
+    )
     delta_ = str(delta_)
 
     with open(delta_filepath, "w") as f:
@@ -27,10 +33,25 @@ def main():
     parser.add_argument("-f", "--other_file", "--file_2", type=str, required=True)
     parser.add_argument("-d", "--delta_file", type=str, required=True)
     parser.add_argument("-o", "--base_dir", "--dir", type=str)
+    parser.add_argument("-m", "--max_diff_length",
+                        help="The delta algorithm tries to find the point where the difference end. "
+                             "This parameter sets the maximum length the algorithem will search the end of a "
+                             "difference. Default is 1000.",
+                        type=int)
+    parser.add_argument("-l", "--min_length_for_fit",
+                        help="This parameter sets the minimum length of identical characters to mark ending of "
+                             "a difference.",
+                        type=int)
     args = parser.parse_args()
 
-    create_delta_file(filepath_1=args.base_file, filepath_2=args.other_file, delta_filepath=args.delta_file,
-                      base_dir=args.base_dir)
+    create_delta_file(
+        filepath_1=args.base_file,
+        filepath_2=args.other_file,
+        delta_filepath=args.delta_file,
+        min_length_for_fit=args.min_length_for_fit,
+        max_diff_length=args.max_diff_length,
+        base_dir=args.base_dir
+    )
 
 
 if __name__ == "__main__":
--- smart_delta/src/delta_generator.py
+++ smart_delta/src/delta_generator.py
@@ -1,15 +1,32 @@
-from typing import Tuple, List
+import time
+from typing import Tuple, List, Optional
 
 from smart_delta.src import (INSERTION_MARK, DELETION_MARK, REPLACEMENT_MARK, )
 from smart_delta.src.delta_element import DeltaElement
 
 
 class DeltaGenerator:
-    def __init__(self, data_0, data_1):
+    DEFAULT_MAX_DIFF_LENGTH = 1000
+    DEFAULT_MIN_LENGTH_FOR_FIT = 3
+
+    def __init__(
+            self,
+            data_0,
+            data_1,
+            min_length_for_fit: Optional[int] = None,
+            max_diff_length: Optional[int] = None
+    ):
         self.data_0 = data_0
         self.data_1 = data_1
-        self.length_for_fitting = 3
-        self.max_diff_length = 50
+        if min_length_for_fit:
+            self.min_length_for_fit = min_length_for_fit
+        else:
+            self.min_length_for_fit = self.DEFAULT_MIN_LENGTH_FOR_FIT
+        if max_diff_length:
+            self.max_diff_length = max_diff_length
+        else:
+            self.max_diff_length = self.DEFAULT_MAX_DIFF_LENGTH
+
         self.delta_elements = self.generate_delta()
 
     def generate_delta(self) -> List[DeltaElement]:
@@ -62,22 +79,58 @@ class DeltaGenerator:
 
         return delta_steps
 
+    # def search_fit(self, data_0, data_1, index_0, index_1):
+
     def range_diff(self, data_0: str, data_1: str) -> Tuple[int, int]:
         fit_index_0, fit_index_1 = len(data_0), len(data_1)
         index_0_for_0, index_1_for_0 = 0, 0
+        index_0_for_1, index_1_for_1 = 0, 0
         res = False
-        while not res and index_0_for_0 <= len(data_0):
-            while index_1_for_0 < self.max_diff_length and index_1_for_0 <= len(data_1):
-                if (data_0[index_0_for_0: index_0_for_0 + self.length_for_fitting] == data_1[
-                                                                                      index_1_for_0: index_1_for_0 + self.length_for_fitting]):
-                    fit_index_0 = index_0_for_0
-                    fit_index_1 = index_1_for_0
-                    res = True
+
+        while not res:
+            while True:
+
+                if index_0_for_1 >= len(data_0) and index_1_for_0 >= len(data_1):
                     break
-                else:
-                    index_1_for_0 += 1
+
+                print("==============\nData_0 for 0: ", data_0[index_0_for_0: index_0_for_0 + self.min_length_for_fit])
+                print("Data_1 for 0: ", data_1[index_1_for_0: index_1_for_0 + self.min_length_for_fit])
+                print("Data_1 for 1: ", data_1[index_1_for_1: index_1_for_1 + self.min_length_for_fit])
+                print("Data_0 for 1: ", data_0[index_0_for_1: index_0_for_1 + self.min_length_for_fit])
+                print(f"{index_0_for_0=} , {index_1_for_0=} | {index_0_for_1=}, {index_1_for_1=}")
+                print("==============\n")
+                # time.sleep(0.01)
+
+                if index_1_for_0 < self.max_diff_length and index_1_for_0 <= len(data_1):
+                    if (
+                            data_0[index_0_for_0: index_0_for_0 + self.min_length_for_fit] ==
+                            data_1[index_1_for_0: index_1_for_0 + self.min_length_for_fit]
+                    ):
+                        fit_index_0 = index_0_for_0
+                        fit_index_1 = index_1_for_0
+                        res = True
+                        break
+                    else:
+                        index_1_for_0 += 1
+
+
+                if index_0_for_1 < self.max_diff_length and index_0_for_1 <= len(data_0):
+                    if (
+                            data_1[index_1_for_1: index_1_for_1 + self.min_length_for_fit] ==
+                            data_0[index_0_for_1: index_0_for_1 + self.min_length_for_fit]
+                    ):
+                        fit_index_0 = index_0_for_1
+                        fit_index_1 = index_1_for_1
+                        res = True
+                        break
+                    else:
+                        index_0_for_1 += 1
+
             index_0_for_0 += 1
             index_1_for_0 = 0
+            index_0_for_1 = 0
+            index_1_for_1 += 1
+        # input()
         return fit_index_0, fit_index_1
 
     def __str__(self):
